b1a1144b70bd0d03e3342eb4d30b529b843183ae
prekira <prekira@hotmail.com>
2018-12-24 21:28:03 -0800

    broke up function into smaller ones

--- PlaylistScraper.rb
+++ PlaylistScraper.rb
@@ -6,14 +6,41 @@ def scraper
 	url = "https://www.billboard.com/charts/hot-100"
 	unparsed_page = HTTParty.get(url)
 	parsed_page = Nokogiri::HTML(unparsed_page)
-	hot100 = parsed_page.css('div.chart-list-item')
-	song_names = Array.new
-	hot100.each do |item|
-		item = item.css('span.chart-list-item__title-text')
-		song_names.push(item.text.tr("\n",""))
-	end
-	puts song_names
+	#get tracks 2-100 on list
+	hot100 = (get_tracks(parsed_page))
+
+	#track names
+	track_names = Array.new
+	#get top track , not included with rest
+	top_track = parsed_page.css('div.chart-number-one__info')
+	#top track name
+	top_track_name = "1: " + (top_track.css('div.chart-number-one__title').text)
+	
+	#add rest of track names
+	#get top track for hot 100, not grouped with rest
+	track_names = get_names(hot100, track_names)
+	puts top_track_name
+	puts track_names
 	#byebug
 end
 
+
+
+def get_names(hot100, track_names)
+	count = 1
+	hot100.each do |item|
+		count += 1
+		track_names.push(count.to_s + ":" + get_name(item))
+	end
+	return track_names
+end
+
+def get_tracks(parsed_page)
+	return parsed_page.css('div.chart-list-item')
+end
+
+def get_name(track)
+	track_name = track.css('span.chart-list-item__title-text').text.tr("\n", "")
+	return track_name
+end
 scraper
